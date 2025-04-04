c368cdbdd53226710d93065d7d50017591262b11
krisvanerum <krisvanerum@gmail.com>
2025-04-04 18:11:55 +0200

    Day 16

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use std::cmp::{Ordering, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 #[derive(Eq, PartialEq, Hash, Clone, Debug)]
 enum Tile {
@@ -6,7 +7,7 @@ enum Tile {
     Empty,
 }
 
-#[derive(Eq, PartialEq, Hash, Clone, Debug)]
+#[derive(Eq, PartialEq, Hash, Clone, Debug, Copy)]
 enum Direction {
     North,
     South,
@@ -14,10 +15,29 @@ enum Direction {
     West,
 }
 
+#[derive(Eq, PartialEq, Clone)]
+struct State {
+    position: (usize, usize),
+    direction: Direction,
+    score: usize,
+    path: HashSet<(usize, usize)>,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.score.cmp(&other.score)
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 fn main() {
     let (map, start_position, end_position) = read_input();
 
-    let mut lowest_score_cache: HashMap<((usize, usize), &Direction), usize> = HashMap::new();
     let turns_map: HashMap<Direction, Vec<Direction>> = [
         (Direction::North, vec![Direction::East, Direction::West]),
         (Direction::East, vec![Direction::South, Direction::North]),
@@ -27,99 +47,71 @@ fn main() {
     .into_iter()
     .collect();
 
-    let mut minimum_score = usize::MAX;
-    step(
-        start_position,
-        &Direction::East,
-        0,
-        &map,
-        &mut lowest_score_cache,
-        &turns_map,
-        end_position,
-        &mut minimum_score,
-    );
+    let start_state = State {
+        position: start_position,
+        direction: Direction::East,
+        score: 0,
+        path: HashSet::from([start_position]),
+    };
 
-    let result = end_position_min_score(&lowest_score_cache, end_position);
+    let mut states = BinaryHeap::new();
+    let mut checked_states_min_scores = HashMap::new();
+    states.push(Reverse(start_state));
 
-    println!("{}", result);
-}
+    let mut minimum_score = None;
+    let mut best_paths = HashSet::new();
 
-fn end_position_min_score(
-    lowest_score_cache: &HashMap<((usize, usize), &Direction), usize>,
-    end_position: (usize, usize),
-) -> usize {
-    lowest_score_cache
-        .iter()
-        .filter(|((position, _), _)| *position == end_position)
-        .map(|(_, &score)| score)
-        .min()
-        .unwrap_or(usize::MAX)
-}
+    while let Some(Reverse(state)) = states.pop() {
+        if state.score > minimum_score.unwrap_or(usize::MAX) {
+            break;
+        }
+        
+        if state.position == end_position {
+            best_paths.extend(state.path.clone());
+            minimum_score = Some(state.score);
+        }
 
-fn step<'a>(
-    position: (usize, usize),
-    direction: &'a Direction,
-    current_score: usize,
-    map: &Vec<Vec<Tile>>,
-    lowest_score_cache: &mut HashMap<((usize, usize), &'a Direction), usize>,
-    turns_map: &'a HashMap<Direction, Vec<Direction>>,
-    end_position: (usize, usize),
-    minimum_score: &mut usize,
-) {
-    if current_score > *minimum_score {
-        return;
+        // add step to next state
+        let next_position = next_position(state.position, &state.direction);
+        let mut path = state.path.clone();
+        path.insert(next_position);
+        let next_state = State {
+            direction: state.direction,
+            position: next_position,
+            score: state.score + 1,
+            path,
+        };
+
+        if map[next_position.0][next_position.1] == Tile::Empty
+            && checked_states_min_scores.get(&(next_state.position, next_state.direction)).unwrap_or(&usize::MAX) >= &next_state.score
+        {
+            checked_states_min_scores.insert((next_state.position, next_state.direction), next_state.score);
+            states.push(Reverse(next_state));
+        }
+
+        // add turns to next state
+        turns_map
+            .get(&state.direction)
+            .expect("next turns")
+            .iter()
+            .for_each(|next_direction| {
+                let next_state = State {
+                    direction: next_direction.clone(),
+                    position: state.position,
+                    score: state.score + 1000,
+                    path: state.path.clone(),
+                };
+
+                if checked_states_min_scores.get(&(next_state.position, next_state.direction)).unwrap_or(&usize::MAX) >= &next_state.score
+                {
+                    checked_states_min_scores.insert((next_state.position, next_state.direction), next_state.score);
+                    states.push(Reverse(next_state));
+                }
+            })
     }
 
-    let old_score = lowest_score_cache
-        .get(&(position, direction))
-        .unwrap_or(&usize::MAX);
-
-    if old_score < &current_score {
-        return;
-    }
-
-    if position == end_position {
-        println!(
-            "looks like we made it to the end with score {}",
-            current_score
-        );
-        *minimum_score = current_score;
-    }
-
-    lowest_score_cache.insert((position, direction), current_score);
-
-    // try step
-    let next_position = next_position(position, direction);
-    if map[next_position.0][next_position.1] == Tile::Empty {
-        step(
-            next_position,
-            direction,
-            current_score + 1,
-            map,
-            lowest_score_cache,
-            turns_map,
-            end_position,
-            minimum_score,
-        );
-    }
-
-    // try turns
-    turns_map
-        .get(&direction)
-        .expect("next turns")
-        .iter()
-        .for_each(|next_direction| {
-            step(
-                position,
-                next_direction,
-                current_score + 1000,
-                map,
-                lowest_score_cache,
-                turns_map,
-                end_position,
-                minimum_score,
-            );
-        })
+    println!("{}", minimum_score.unwrap());
+    println!("{}", best_paths.len());
 }
 
 fn next_position(position: (usize, usize), direction: &Direction) -> (usize, usize) {
